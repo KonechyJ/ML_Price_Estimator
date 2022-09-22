c9034754cfba984391c0ffb52d218ae1ca624939
Unknown <Jkonechy@live.com>
2022-09-22 14:14:38 -0700

    Started the process of Adding SQL database and login system
    Just doing a quick commit and push before I change IDE

--- app.py
+++ app.py
@@ -1,7 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
+
+
 #app.debug = True
 app = Flask(__name__)
+db = SQLAlchemy(app)
+app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///database.db'
+#change the secret key later
+app.config["SECRET_KEY"] = "SECRETKEY123"
 
+#creates the table for our database
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), nullable=False)
+    password = db.Column(db.String(80), nullable=False)
+
+#various routes to html files
 @app.route("/")
 def index():
     return render_template("home.html")
@@ -9,6 +24,23 @@ def index():
 @app.route("/about")
 def about():
     return render_template("about.html")
+
+@app.route("/estimator")
+def estimator():
+    return render_template("estimator.html")
+
+@app.route("/home")
+def home():
+    return render_template("home.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
 
 if __name__ == "__main__":
     app.run()
