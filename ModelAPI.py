0c228e481eedf0a57f567647ab5b0cd6ecde48b8
arif599 <arifullashaik01@gmail.com>
2022-10-12 13:07:35 -0700

    adding skeleton class for ModelAPI

---
+++ ModelAPI.py
@@ -0,0 +1,31 @@
+class ModelAPI:
+    def __init__(self, model):
+        pass
+
+    def predict(self, data):
+        pass
+
+    def train(self, data):
+        pass
+
+    def performance(self, data):
+        pass
+
+    def save(self, path):
+        pass
+
+    def load(self, path):
+        pass
+
+    def get_attributes(self):
+        pass
+
+    def get_description(self):
+        pass
+
+
+
+
+
+
+
